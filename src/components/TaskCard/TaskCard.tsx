9bd24016df435d00037ed1769e142dc69c189c59
eexxiist <mego_dilya@mail.ru>
2026-03-22 21:51:22 +0300

    создан компонент

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,1 +1,10 @@
+import type { TaskCardProps } from "../../types";
 
+export const TaskCard = ({ task }: TaskCardProps) => {
+  return (
+    <div>
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+    </div>
+  );
+};
