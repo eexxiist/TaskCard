032c8aca357694b83b35e13bfb2357db3a8a29c2
eexxiist <mego_dilya@mail.ru>
2026-03-22 20:54:03 +0300

    добавлены типы

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type TaskStatus = "todo" | "in-progress" | "done";
+export type TaskPriority = "low" | "medium" | "high";
+
+export interface Task {
+  title: string;
+  description: string;
+  priority: TaskPriority;
+  status: TaskStatus;
+}
