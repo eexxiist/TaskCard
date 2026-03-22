6b56c3e2ea290c008b2180bd4e24c7e552cdab7d
eexxiist <mego_dilya@mail.ru>
2026-03-22 23:10:42 +0300

    добавлен приоритет и стили

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { TaskCardProps, TaskStatus } from "../../types";
+import "../TaskCard.css";
 
 export const TaskCard = ({ task }: TaskCardProps) => {
     const [status, setStatus] = useState<TaskStatus>(task.status);
@@ -23,7 +24,9 @@ export const TaskCard = ({ task }: TaskCardProps) => {
         <div>
             <h3>{task.title}</h3>
             <p>{task.description}</p>
+            <span>{task.priority}</span>
             <span>{status}</span>
+
             <button onClick={handleNextStatus} disabled={isDone}>
                 Next status
             </button>
