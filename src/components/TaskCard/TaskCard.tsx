d98e9976877b38c2f921c2289261b734853471c5
eexxiist <mego_dilya@mail.ru>
2026-03-22 22:46:39 +0300

    добавлен стейт статуса

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,10 +1,13 @@
-import type { TaskCardProps } from "../../types";
+import { useState } from "react";
+import type { TaskCardProps, TaskStatus } from "../../types";
 
 export const TaskCard = ({ task }: TaskCardProps) => {
+    const [status, setStatus] = useState<TaskStatus>(task.status)
   return (
     <div>
       <h3>{task.title}</h3>
       <p>{task.description}</p>
+      <span>{status}</span>
     </div>
   );
 };
